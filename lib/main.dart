0163babc29b3544537bbeeaa81eb8bc90961b218
khan9920 <shafkhanmohamed23@gmail.com>
2020-11-08 17:57:03 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What is your favourite color?',
+      'What is your favourite animal?'
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('My First App'),
+          ),
+          body: Column(
+            children: [
+              Text('The question'),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: null,
+              )
+            ],
+          )),
+    );
+  }
+}
